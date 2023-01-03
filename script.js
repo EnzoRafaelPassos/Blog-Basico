10472c1e9ee0ea4e7ae45dfb10be02a8e9d50899
Enzo Rafael Passos <enzorafel14@gmail.com>
2023-01-03 14:18:04 -0300

    Adicionando a opcao de like

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 const icon = document.querySelector("#icon");
+const iconDois = document.querySelector("#iconDois");
+const iconTres = document.querySelector("#iconTres");
+const iconQuatro = document.querySelector("#iconQuatro");
 
 icon.onclick = () => {
-  icon.classList.toggle("filled");
+    icon.classList.toggle("filled");
 };
-    
+
+iconDois.onclick = () => {
+    iconDois.classList.toggle("filled");
+};
+
+iconTres.onclick = () => {
+    iconTres.classList.toggle("filled");
+};
+
+iconQuatro.onclick = () => {
+    iconQuatro.classList.toggle("filled");
+};
